1050ceda0b32f567498a07eb49f2a4049ffe2668
sofiasmiv <crp23gdu@uea.ac.uk>
2023-12-08 13:01:05 +0000

    Update MovieDatabase.java

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -1,8 +1,11 @@
+import javax.swing.text.html.StyleSheet;
 import java.lang.*;
 import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.*;
+
+import static java.util.Collections.reverse;
 
 public class MovieDatabase {
 
@@ -26,6 +29,14 @@ public class MovieDatabase {
             e.printStackTrace();
         }
 
+    }
+
+    public void sortDuration(){
+        Collections.sort(movieIndex);
+        for(Movie other:movieIndex){
+
+        }
+        reverse(movieIndex);
     }
 
 
