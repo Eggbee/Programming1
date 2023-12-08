fc56e605d7a5561c5a435406666775c5d08bdf9c
sofiasmiv <crp23gdu@uea.ac.uk>
2023-12-08 11:55:18 +0000

    Update MovieDatabase.java

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -6,17 +6,8 @@ import java.util.*;
 
 public class MovieDatabase {
 
-    // MovieDatabase(){
-    //     String name = movieList[0];
-    //     String releaseDate = movieList[1];
-    //     String certificate = movieList[2];
-    //     String genre = movieList[3];
-    //     String duration = movieList[4];
-    //     String rating = movieList[5];
-    // }
-
     static ArrayList<Movie> movieIndex = new ArrayList<>();
-    private static String[] movieList = new String[6];
+
 
     public static void main(String[] args) { //throws java.io.IOException{
 
@@ -24,28 +15,19 @@ public class MovieDatabase {
             BufferedReader myData = new BufferedReader(new FileReader("movies.txt"));
             String line;
             while((line = myData.readLine()) != null) {
-                //System.out.println(line);
+//                System.out.println(line);
                 Movie newmovie = new Movie(line);
-                //System.out.println(newmovie);
+//                System.out.println(newmovie);
                 movieIndex.add(newmovie);
-
-                //add movies to list
-//                for (int i = 1; i <= 6; i++) {
-//                    //potentially remove/ add ""
-//                    movieList[i] = fields[i];
-//                }
-                //pass through constructor
-                //potentially add object name to movieindex, potentially with a key
-                //clear movielist
-
             }
+//            System.out.println(movieIndex);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-//    MovieDatabase[] MovieList;
+
 
 
 }
